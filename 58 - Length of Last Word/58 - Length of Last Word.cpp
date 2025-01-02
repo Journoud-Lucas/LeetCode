fa2bb6199a4f36addaba3e6a6b0a34086402f914
Journoud-Lucas <journoud.lucas@gmail.com>
2025-01-02 17:20:32 +0100

    • Problem 58 solved

---
+++ 58 - Length of Last Word/58 - Length of Last Word.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+int lengthOfLastWord(std::string s);
+
+int main()
+{
+    std::cout << lengthOfLastWord("Hello World") << std::endl;
+    std::cout << lengthOfLastWord("   fly me   to   the moon  ") << std::endl;
+    std::cout << lengthOfLastWord("luffy is still joyboy") << std::endl;
+
+    return 0;
+}
+
+int lengthOfLastWord(std::string s)
+{
+    int size = 0;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        if (s[i] == ' ' && size)
+        {
+            return size;
+        }
+        if (s[i] != ' ')
+        {
+            size++;
+        }
+    }
+    return size;
+}
