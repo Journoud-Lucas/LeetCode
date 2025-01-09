5939c6a20a2cd87f37c0f66d91a7ccc06fd69b88
Journoud-Lucas <journoud.lucas@gmail.com>
2025-01-09 15:38:14 +0100

    • 13 more fast

--- 13 - Roman to Integer/13 - Roman to Integer.cpp
+++ 13 - Roman to Integer/13 - Roman to Integer.cpp
@@ -23,8 +23,8 @@ int main()
 int romanToInt(std::string s)
 {
     std::unordered_map<char, int> symbole{ {'I', 1}, {'V', 5}, {'X', 10}, {'L',50}, {'C',100},{'D',500},{'M',1000} };
-    int somme = 0;
-    for (int i = 0; i < s.length(); i++)
+    int somme = symbole[s[s.size() - 1]];
+    for (int i = 0; i < s.size() - 1; i++)
     {
         if (symbole[s[i + 1]] > symbole[s[i]])
         {
